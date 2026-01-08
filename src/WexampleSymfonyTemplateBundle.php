5f6973bf2ee791882e8766a905b34ee10d8aa773
weeger <romain.weeger@gmail.com>
2026-01-08 13:49:14 +0100

    Config

--- src/WexampleSymfonyTemplateBundle.php
+++ src/WexampleSymfonyTemplateBundle.php
@@ -3,7 +3,15 @@
 namespace Wexample\SymfonyTemplate;
 
 use Wexample\SymfonyHelpers\Class\AbstractBundle;
+use Wexample\SymfonyHelpers\Helper\BundleHelper;
+use Wexample\SymfonyLoader\Interface\LoaderBundleInterface;
 
-class WexampleSymfonyTemplateBundle extends AbstractBundle
+class WexampleSymfonyTemplateBundle extends AbstractBundle implements LoaderBundleInterface
 {
+    public static function getLoaderFrontPaths(): array
+    {
+        return [
+            BundleHelper::getBundleCssAlias(static::class) => __DIR__.'/../assets/',
+        ];
+    }
 }
