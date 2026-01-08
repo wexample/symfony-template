b53e89d1cd92d4240bf4659459d1a8b0a12e6225
weeger <romain.weeger@gmail.com>
2026-01-08 14:18:25 +0100

    Icons service

--- src/Service/IconService.php
+++ src/Service/IconService.php
@@ -2,50 +2,19 @@
 
 namespace Wexample\SymfonyTemplate\Service;
 
-use DOMDocument;
 use Exception;
-use Psr\Cache\CacheItemInterface;
-use Psr\Cache\CacheItemPoolInterface;
-use stdClass;
-use Symfony\Component\HttpKernel\KernelInterface;
 use Twig\Environment;
-use Wexample\Helpers\Helper\TextHelper;
 use Wexample\SymfonyHelpers\Helper\DomHelper;
-use Wexample\SymfonyHelpers\Helper\FileHelper;
+use Wexample\SymfonyTemplate\Service\Icon\AbstractIconManager;
+use Wexample\SymfonyTemplate\Service\Icon\FaIconManager;
+use Wexample\SymfonyTemplate\Service\Icon\MaterialIconManager;
 
 class IconService
 {
-    private const ICONS_LIBRARY_FA = 'fa';
-    private const ICONS_LIBRARY_MATERIAL = 'material';
-    public const LIBRARY_SEPARATOR = ':';
-
-    protected stdClass $icons;
-    private string $projectDir;
-    private CacheItemInterface $cacheItem;
-
     public function __construct(
-        KernelInterface $kernel,
-        protected readonly CacheItemPoolInterface $cache
+        private readonly FaIconManager $faIconManager,
+        private readonly MaterialIconManager $materialIconManager
     ) {
-        $this->projectDir = $kernel->getProjectDir();
-        $this->cacheItem = $this->cache->getItem('symfony_design_system_icons_list');
-
-        if (! $this->cacheItem->isHit()) {
-            $this->icons = (object) [
-                self::ICONS_LIBRARY_FA => $this->buildIconsListFa(),
-                self::ICONS_LIBRARY_MATERIAL => $this->buildIconsListMaterial(),
-            ];
-            $this->saveRegistryCache();
-        } else {
-            $this->icons = $this->cacheItem->get();
-        }
-    }
-
-    private function saveRegistryCache(): void
-    {
-        $this->cache->save(
-            $this->cacheItem->set($this->icons)
-        );
     }
 
     /**
@@ -64,7 +33,7 @@ class IconService
         $tagName = 'i',
         $type = null
     ) {
-        [$prefix, $icon] = array_pad(explode(self::LIBRARY_SEPARATOR, $name, 2), 2, '');
+        [$prefix, $icon] = array_pad(explode(AbstractIconManager::LIBRARY_SEPARATOR, $name, 2), 2, '');
 
         $lib = $type ?? $prefix;
         $class = trim($class);
@@ -72,29 +41,18 @@ class IconService
 
         // Material Icons
         if (
-            (self::ICONS_LIBRARY_MATERIAL === $lib || ($lib === null && null === $type)
-                && isset($this->icons->{self::ICONS_LIBRARY_MATERIAL}[$icon]))
+            ($this->materialIconManager->getLibraryKey() === $lib || ($lib === null && null === $type))
+            && $this->materialIconManager->hasIcon($icon)
         ) {
-            return
-                '<' . $tagName .
-                ' class="' . $baseClass . ' material-icons">' .
-                $icon .
-                '</' . $tagName . '>';
+            return $this->materialIconManager->iconTag($icon, $baseClass, $tagName);
         }
 
         // Font Awesome
         if (
-            (self::ICONS_LIBRARY_FA === $lib || ($lib === null && null === $type))
-            && isset($this->icons->{self::ICONS_LIBRARY_FA}[$icon])
+            ($this->faIconManager->getLibraryKey() === $lib || ($lib === null && null === $type))
+            && $this->faIconManager->hasIcon($icon)
         ) {
-            $classes = $this->icons->{self::ICONS_LIBRARY_FA}[$icon]["name"];
-            $classes = 'fa-' . str_replace('/', ' fa-', $classes);
-
-            return
-                '<' . $tagName .
-                ' class="' . $baseClass . '">' .
-                '<i class="fa ' . $classes . '"></i>' .
-                '</' . $tagName . '>';
+            return $this->faIconManager->iconTag($icon, $baseClass, $tagName);
         }
 
         return
@@ -102,71 +60,6 @@ class IconService
             ' class="icon">' .
             $name .
             '</' . $tagName . '>';
-    }
-
-    public function buildIconsListMaterial(): array
-    {
-        $output = [];
-        $pathFonts = $this->projectDir . '/node_modules/material-design-icons/';
-
-        if (is_dir($pathFonts)) {
-            foreach (scandir($pathFonts) as $item) {
-                $pathSvg = $pathFonts . $item . '/svg/production/';
-                if ($item[0] !== '.' && is_dir($pathFonts . $item) && file_exists($pathSvg)) {
-                    foreach (scandir($pathSvg) as $fileIcon) {
-                        if ($fileIcon[0] !== '.') {
-                            $prefix = 'ic_';
-                            $suffix = '_24px.svg';
-                            if (str_starts_with($fileIcon, $prefix) && str_ends_with($fileIcon, $suffix)) {
-                                $iconName = TextHelper::removePrefix(
-                                    TextHelper::removeSuffix(
-                                        $fileIcon,
-                                        $suffix
-                                    ),
-                                    $prefix
-                                );
-
-                                $output[$iconName] = [
-                                    'content' => null,
-                                    'file' => $pathSvg . $fileIcon,
-                                    'name' => $iconName,
-                                ];
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        return $output;
-    }
-
-    public function buildIconsListFa(): array
-    {
-        $pathSvg = $this->projectDir . '/vendor/fortawesome/font-awesome/svgs/';
-        $output = [];
-
-        if (is_dir($pathSvg)) {
-            $groups = scandir($pathSvg);
-
-            foreach ($groups as $group) {
-                if ('.' !== $group[0]) {
-                    $icons = scandir($pathSvg . $group);
-                    foreach ($icons as $fileIcon) {
-                        if ('.' !== $fileIcon[0]) {
-                            $iconName = $group . '/' . FileHelper::removeExtension(basename($fileIcon));
-                            $output[$iconName] = [
-                                'name' => $iconName,
-                                'file' => $pathSvg . $group . '/' . $fileIcon,
-                                'content' => null,
-                            ];
-                        }
-                    }
-                }
-            }
-        }
-
-        return $output;
     }
 
     /**
@@ -178,73 +71,17 @@ class IconService
         array $classes = []
     ): string {
         $default = DomHelper::buildTag('span');
+        [$type, $icon] = array_pad(explode(AbstractIconManager::LIBRARY_SEPARATOR, $name, 2), 2, '');
 
-        if ($icon = $this->loadIconSvg(self::ICONS_LIBRARY_MATERIAL, $name, $classes)) {
-            return $icon;
+        if ($type === $this->materialIconManager->getLibraryKey()) {
+            return $this->materialIconManager->iconSource($twig, $icon, $classes) ?? $default;
         }
 
-        if ($icon = $this->loadIconSvg(self::ICONS_LIBRARY_FA, $name, $classes)) {
-            return $icon;
+        if ($type === $this->faIconManager->getLibraryKey()) {
+            return $this->faIconManager->iconSource($twig, $icon, $classes) ?? $default;
         }
 
         return $default;
-    }
-
-    private function loadIconSvg(
-        string $registryType,
-        string $name,
-        array $classes
-    ): ?string {
-        [$type, $name] = explode(
-            self::LIBRARY_SEPARATOR,
-            $name
-        );
-
-        if ($registryType !== $type) {
-            return null;
-        }
-
-        $registry = &$this->icons->$registryType;
-        $contentName = md5(
-            implode(
-                $classes
-            )
-        );
-
-        if (isset($registry[$name])) {
-            if (! isset($registry[$name]['content'][$contentName])) {
-                $svgContent = file_get_contents($registry[$name]['file']);
-
-                $dom = new DOMDocument();
-                $dom->loadXML($svgContent);
-                $tags = $dom->getElementsByTagName('svg');
-                if ($tags->length > 0) {
-                    $svg = $tags->item(0);
-
-                    $existingClass = $svg->getAttribute('class');
-                    $svg->setAttribute(
-                        'class',
-                        $existingClass
-                        . implode(
-                            ' ',
-                            array_merge(
-                                ['icon'],
-                                $classes
-                            )
-                        )
-                    );
-
-                    $content = $dom->saveXML($svg);
-                    $registry[$name]['content'][$contentName] = $content;
-
-                    $this->saveRegistryCache();
-                }
-            }
-
-            return $registry[$name]['content'][$contentName];
-        }
-
-        return null;
     }
 
     /**
@@ -253,8 +90,8 @@ class IconService
     public function iconList(string $type): array
     {
         return match ($type) {
-            self::ICONS_LIBRARY_FA => $this->buildIconsListFa(),
-            self::ICONS_LIBRARY_MATERIAL => $this->buildIconsListMaterial(),
+            $this->faIconManager->getLibraryKey() => $this->faIconManager->iconList(),
+            $this->materialIconManager->getLibraryKey() => $this->materialIconManager->iconList(),
             default => [],
         };
     }
